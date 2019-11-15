0ae5d5a45cdd8ddbb0eeb763720ca247995000b8
PMiskew <paul.miskew@gmail.com>
2019-11-15 11:19:06 -0500

    test

--- src/Controller.java
+++ src/Controller.java
@@ -1,14 +1,37 @@
+import java.util.ArrayList;
 
 public class Controller {
 
 	
+	//GUI CLASSES
 	WindowA mainWindow = new WindowA();
 	WindowB winB = new WindowB();
 	
+	//DATA CLASSES
+	ArrayList<Student> students = new ArrayList<Student>();
+	
+	public Controller() {
+		
+		//LOAD ALL STUDENTS INTO list student
+	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Controller c = new Controller();
+	}
+	
+	public void activateWindow(int a) {
+		
+		#set all to false
+		mainWindow.setVisible(false);
+		winB.setVisible(false);
+		
+		if (a == 0) {
+			mainWindow.setVisible(true);
+		}
+		else if (a == 1) {
+			winB.setVisible(true);
+		}
 	}
 
 }
